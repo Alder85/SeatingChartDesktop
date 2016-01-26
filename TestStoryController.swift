eb92c3e23cf849170f5f603de90d1f4ca99e0a70
WENDOLEK, CONNOR <732408@ehs-s-itc-16.local>
2016-01-26 10:28:12 -0600

    Initial Commit

---
+++ TestStoryController.swift
@@ -0,0 +1,27 @@
+//
+//  TestStoryController.swift
+//  DriveSample
+//
+//  Created by WENDOLEK, CONNOR on 1/20/16.
+//
+//
+
+
+import Cocoa
+
+class TestStoryController: NSViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+}
+
